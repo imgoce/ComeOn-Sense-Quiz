84b1104003d9766522b428ccfe376388d46cc9c7
seungchan0810 <imgoce@gmail.com>
2023-11-22 17:13:38 +0900

    주석 삭제

--- ComeOnSenseQuiz.c
+++ ComeOnSenseQuiz.c
@@ -42,6 +42,7 @@ int main() {
             cls;
             if (i == 1) {
                 int n = mainDraw();
+                
                 if (n == 0) {
                     //시사
                     cls;
@@ -312,112 +313,6 @@ int mainDraw() {
         }
    }
 }
-
-
-
-/* #define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct Member {
-    char username[50];
-};
-
-void signup();
-void login();
-int isUsernameExists(const char* username);
-
-int main() {
-    int choice;
-
-    do {
-        printf("\n1. 회원 가입\n");
-        printf("2. 로그인\n");
-        printf("3. 종료\n");
-        printf("선택: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-        case 1:
-            signup();
-            break;
-        case 2:
-            login();
-            break;
-        case 3:
-            printf("프로그램을 종료합니다.\n");
-            break;
-        default:
-            printf("올바른 선택이 아닙니다. 다시 선택해주세요.\n");
-        }
-    } while (choice != 3);
-
-    return 0;
-}
-
-void signup() {
-    struct Member member;
-    FILE* file;
-
-    printf("아이디: ");
-    scanf("%s", member.username);
-
-    // 아이디 중복 확인
-    if (isUsernameExists(member.username)) {
-        printf("이미 존재하는 아이디입니다. 다른 아이디를 선택해주세요.\n");
-        return;
-    }
-
-    file = fopen("members.txt", "a");
-    if (file != NULL) {
-        fprintf(file, "%s\n", member.username);
-        fclose(file);
-        printf("회원 가입이 완료되었습니다.\n");
-    }
-    else {
-        printf("파일을 열 수 없습니다.\n");
-    }
-}
-
-void login() {
-    struct Member member;
-    char inputUsername[50];
-    FILE* file;
-
-    printf("아이디: ");
-    scanf("%s", inputUsername);
-
-    file = fopen("members.txt", "r");
-    if (file != NULL) {
-        while (fscanf(file, "%s", member.username) == 1) {
-            if (strcmp(member.username, inputUsername) == 0) {
-                fclose(file);
-                printf("로그인 성공!\n");
-                return;
-            }
-        }
-        fclose(file);
-    }
-
-    printf("로그인 실패. 아이디가 올바르지 않습니다.\n");
-}
-
-int isUsernameExists(const char* username) {
-    FILE* file = fopen("members.txt", "r");
-    if (file != NULL) {
-        char existingUsername[50];
-        while (fscanf(file, "%s", existingUsername) == 1) {
-            if (strcmp(existingUsername, username) == 0) {
-                fclose(file);
-                return 1; // 아이디가 이미 존재함
-            }
-        }
-        fclose(file);
-    }
-    return 0; // 아이디가 존재하지 않음
-}*/
 
 /* 파일에 저장된 문제를 랜덤으로 출력 후 엔터를 누르면 다음 문제로 넘어감
 #define _CRT_SECURE_NO_WARNINGS
