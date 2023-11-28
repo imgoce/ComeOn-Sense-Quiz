4d37ca21ac7de92acf38f984243c2f4795d6511e
seungchan0810 <imgoce@gmail.com>
2023-11-28 16:48:26 +0900

    shuffle 함수 추가, 문제를 랜덤으로 출력함

--- ComeOnSenseQuiz.c
+++ ComeOnSenseQuiz.c
@@ -391,6 +391,14 @@ int level() {
         }
     }
 }
+void shuffleArray(int arr[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
 
 /*int main() {
     FILE* file;
