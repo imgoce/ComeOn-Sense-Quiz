07dee4a2a55e3031d4fefbae7fccc774b2e3b4ad
APEOR <kimym2004@hanmail.net>
2023-12-09 16:00:34 +0900

    1을 입력하면 문제 출력, 2를 입력하면 오답노트 출력

--- ComeOnSenseQuiz.c
+++ ComeOnSenseQuiz.c
@@ -1702,7 +1702,7 @@ void displayHint(char hints[][MAX_HINT_LENGTH], int currentQuestionIndex) {
 
 */
 
-/* 4를 누르면 오답노트 출력하는 코드
+/* 1을 입력하면 문제 출력, 2를 입력하면 오답노트 출력
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -1721,7 +1721,6 @@ void displayHint(char hints[][MAX_HINT_LENGTH], int currentQuestionIndex);
 int isAlreadyRecorded(char* filename, char question[MAX_QUESTION_LENGTH]);
 
 struct Member {
-    char username[50];
     int numQuestionsSolved; // 푼 문제 개수
     char grade;
 };
@@ -1745,38 +1744,26 @@ void displayWrongAnswers() {
     fclose(wrongAnswersFile);
 }
 
-void signup();
-void login();
-int isUsernameExists(const char* username);
-
 int main() {
     struct Member member;
     int choice;
 
     do {
-        printf("\n1. 회원 가입\n");
-        printf("2. 로그인\n");
-        printf("3. 문제로 넘어가기\n");
-        printf("4. 오답노트\n");
+        printf("1. 문제로 넘어가기\n");
+        printf("2. 오답노트\n");
         printf("선택: ");
         scanf("%d", &choice);
 
         switch (choice) {
         case 1:
-            signup();
             break;
         case 2:
-            login();
-            break;
-        case 3:
-            break;
-        case 4:
             displayWrongAnswers();
             break;
         default:
             printf("올바른 선택이 아닙니다. 다시 선택해주세요.\n");
         }
-    } while (choice != 3 || choice == 4);
+    } while (choice != 1);
 
     char questionFilename[] = "question.txt";
     char answerFilename[] = "answer.txt";
@@ -1917,69 +1904,6 @@ int main() {
 }
 
 
-void signup() {
-    struct Member member;
-    FILE* file;
-
-    printf("아이디: ");
-    scanf("%s", member.username);
-
-    // 아이디 중복 확인
-    if (isUsernameExists(member.username)) {
-        printf("이미 존재하는 아이디입니다. 다른 아이디를 선택해주세요.\n");
-        return;
-    }
-
-    file = fopen("members.txt", "a");
-    if (file != NULL) {
-        fprintf(file, "%s\n", member.username);
-        fclose(file);
-        printf("회원 가입이 완료되었습니다.\n");
-    }
-    else {
-        printf("파일을 열 수 없습니다.\n");
-    }
-}
-
-void login() {
-    struct Member member;
-    char inputUsername[50];
-    FILE* file;
-
-    printf("아이디: ");
-    scanf("%s", inputUsername);
-
-    file = fopen("members.txt", "r");
-    if (file != NULL) {
-        while (fscanf(file, "%s", member.username) == 1) {
-            if (strcmp(member.username, inputUsername) == 0) {
-                fclose(file);
-                printf("로그인 성공!\n");
-                return;
-            }
-        }
-        fclose(file);
-    }
-
-    printf("로그인 실패. 아이디가 올바르지 않습니다.\n");
-}
-
-
-int isUsernameExists(const char* username) {
-    FILE* file = fopen("members.txt", "r");
-    if (file != NULL) {
-        char existingUsername[50];
-        while (fscanf(file, "%s", existingUsername) == 1) {
-            if (strcmp(existingUsername, username) == 0) {
-                fclose(file);
-                return 1; // 아이디가 이미 존재함
-            }
-        }
-        fclose(file);
-    }
-    return 0; // 아이디가 존재하지 않음
-}
-
 void readQuestionAndAnswer(char* questionFilename, char* answerFilename, char* hintFilename, char questions[][MAX_QUESTION_LENGTH], char answers[][MAX_ANSWER_LENGTH], char hints[][MAX_HINT_LENGTH], int* numQuestions) {
     FILE* questionFile = fopen(questionFilename, "r");
     FILE* answerFile = fopen(answerFilename, "r");
@@ -2048,5 +1972,6 @@ int isAlreadyRecorded(char* filename, char question[MAX_QUESTION_LENGTH]) {
     fclose(file);
     return 0; // 중복 기록이 발견되지 않음
 }
+
 
 */
