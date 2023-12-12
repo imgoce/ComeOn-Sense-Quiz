81a9daa2e09efb0d4c178bbed70e438b91524125
seungchan0810 <imgoce@gmail.com>
2023-12-12 16:37:47 +0900

    함수 모듈화

---
+++ util.h
@@ -0,0 +1,10 @@
+/* 헤더 선언 */
+#pragma once
+#include "main.h"
+
+/* 함수 선언 */
+void init(); //크기 설정
+void setcolor(int, int); //색상
+void fullscreen(); //풀 스크린
+void gotoxy(int, int); //좌표 이동
+int keyControl(); //키 조작 설정
